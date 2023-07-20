de1845d7ba8b873c38c83b83f82358f3024f0d14
Matheus Assunção <matheus.assuncao994@gmail.com>
2023-07-19 22:29:14 -0400

    Criação de Mock e model Frase além da criação de string interpolation

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.scss']
 })
-export class TentativasComponent {
+export class TentativasComponent implements OnInit{
+
+  public coracaoVazio: string = '/assets/coracao_vazio.png'
+  public coracaoCheio: string = '/assets/coracao_cheio.png'
+
+  constructor(){}
+
+  ngOnInit() {
+    
+  }
 
 }
